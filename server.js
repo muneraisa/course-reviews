b08037fdd4f04155b683f4ea8d476850243b91e6
muneraisa <muneeraisa33@gmail.com>
2025-07-11 15:11:45 +0300

    Landing page, CSS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
+app.use(express.static('public'));
 
 // Middlewares
 const mongoose = require("mongoose"); //databases
